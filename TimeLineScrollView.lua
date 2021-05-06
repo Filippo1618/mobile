4205dfe2c85638a8c6a77f1c0a2b7bb437ff447a
WaFlaco <falcone.valerioag@gmail.com>
2021-05-06 11:09:59 +0200

    mod_tlsw

--- TimeLineScrollView.lua
+++ TimeLineScrollView.lua
@@ -80,43 +80,35 @@ local function createVsGroup(lvl_selected)
   char3.x = char2.x + 90
   table.insert(vsTeamTable,char3)
 
-end 
+end
 
 local function onObjectTap( event )
-      
   --se la vsTeamTable non è vuota allora la pulisco
   if(#vsTeamTable ~= 0) then
-  
+
     local char
-    
+
     for i = #vsTeamTable,1,-1 do
       char = vsTeamTable[i]
       table.remove(vsTeamTable,i)
       display.remove(char)
       char = nil
-    
+
     end
   end
-  
   createVsGroup(lvl_selected)
-      
   return true
 end
 
 
 local function letsPlay(event)
-  
+
   local phase = event.phase
-      
   if (phase == "began") then
-    
     print("event.target.id = "..event.target.id)
     print("event.target.label = "..event.target.nome)
-
   elseif (phase == "ended") then
-  
-    print("Livello selezionato:\n".. lvl_selected
-.. "!!\nsi giocaaaaa!")
+    print("Livello selezionato:\n"..lvl_selected.."!!\nsi giocaaaaa!")
     composer.setVariable("vsTeamTable",vsTeamTable)
     composer.gotoScene("game",{effect ="fade",time = 400})
   end
@@ -124,187 +116,184 @@ local function letsPlay(event)
 end
 
 local function previousPage()
-    composer.gotoScene("menu",{effect = "fade", time = 200})
+  print("back to the menu")
+  composer.gotoScene("menu",{effect = "fade", time = 200})
 end
-
 
 ------------------------------------------------------------------------------------
 
-
-
 -- Create scene
-
 function scene:create( event )
 
-local sceneGroup = self.view
+  local sceneGroup = self.view
 
-backGroup = display.newGroup()
-sceneGroup:insert(backGroup)
+  backGroup = display.newGroup()
+  sceneGroup:insert(backGroup)
 
-mainGroup = display.newGroup()
-sceneGroup:insert(mainGroup)
+  mainGroup = display.newGroup()
+  sceneGroup:insert(mainGroup)
 
-uiGroup = display.newGroup()
-sceneGroup:insert(uiGroup)
+  uiGroup = display.newGroup()
+  sceneGroup:insert(uiGroup)
 
-local background  = display.newImageRect("img/sfondo_spacecartoon.jpg", 480*1.2,320*1.2)
-background.x = display.contentCenterX
-background.y = display.contentCenterY
-backGroup:insert(background)
+  local background  = display.newImageRect("img/sfondo_spacecartoon.jpg", 480*1.2,320*1.2)
+  background.x = display.contentCenterX
+  background.y = display.contentCenterY
+  backGroup:insert(background)
 
-vsTeamGroup = display.newGroup()
-vsTeamGroup.x = 100
-vsTeamGroup.y = 240
-sceneGroup:insert( vsTeamGroup )
+  vsTeamGroup = display.newGroup()
+  vsTeamGroup.x = 100
+  vsTeamGroup.y = 240
+  sceneGroup:insert( vsTeamGroup )
 
 
--- Create a scrollView
-local scrollView = widget.newScrollView (
+  -- Create a scrollView
+  local scrollView = widget.newScrollView (
+    {
+      width = display.contentWidth*1.2,
+      height = display.contentHeight,
+      left = -40,
+      backgroundColor = backgroundColor,
+      --isBounceEnabled = false,
+      horizontalScrollDisabled = false,
+      verticalScrollDisabled = true,
+      hideBackground = true,
+      rightPadding = 100,
+      listener = scrollListener
+    }
+  )
+  backGroup:insert( scrollView )
+
+  --pulsantiLivelli
+  local buttonMedioevo = widget.newButton(
   {
-    width = display.contentWidth*1.2,
-    height = display.contentHeight,
-    left = -40,
-    backgroundColor = backgroundColor,
-    --isBounceEnabled = false,
-    horizontalScrollDisabled = false,
-    verticalScrollDisabled = true,
-    hideBackground = true,
-    rightPadding = 100,
-    listener = scrollListener
+      width = lunghezzaLivelli,
+      height = altezzaLivelli,
+      label = "Medioevo 1000 D.C.",
+      textAllign ="center",
+      labelColor = { default={ 0.2, 0.2, 0.2 }, over={ 0, 0, 0, 0.5 } },
+      labelYOffset = altezzaLivelli/2 + 20,
+      top = 20,
+      left = 20,
+      defaultFile ="img/sfondo_medioevo.png",
+      onPress = function() lvl_selected= "medioevo"; composer.setVariable("lvl_selected",lvl_selected) ;end
   }
-)
-backGroup:insert( scrollView )
+  )
+  buttonMedioevo:addEventListener( "tap", onObjectTap )
 
---pulsantiLivelli
-local buttonMedioevo = widget.newButton(
-{
+  local buttonRinascimento = widget.newButton(
+  {
     width = lunghezzaLivelli,
     height = altezzaLivelli,
-    label = "Medioevo 1000 D.C.",
+    label = "Rinascimento 1500 D.C.",
     textAllign ="center",
-    labelColor = { default={ 0.2, 0.2, 0.2 }, over={ 0, 0, 0, 0.5 } },
+    labelColor = { default={ 0.2, 0.2, 0.2  }, over={ 0, 0, 0, 0.5 } },
     labelYOffset = altezzaLivelli/2 + 20,
     top = 20,
     left = 20,
-    defaultFile ="img/sfondo_medioevo.png",
-    onPress = function() lvl_selected= "medioevo"; composer.setVariable("lvl_selected",lvl_selected) ;end
-}
-)
-buttonMedioevo:addEventListener( "tap", onObjectTap )
-
-local buttonRinascimento = widget.newButton(
-{
-  width = lunghezzaLivelli,
-  height = altezzaLivelli,
-  label = "Rinascimento 1500 D.C.",
-  textAllign ="center",
-  labelColor = { default={ 0.2, 0.2, 0.2  }, over={ 0, 0, 0, 0.5 } },
-  labelYOffset = altezzaLivelli/2 + 20,
-  top = 20,
-  left = 20,
-  defaultFile ="img/sfondo_rinascimento.png",
-  onPress = function() lvl_selected
-= "rinascimento";composer.setVariable("lvl_selected",lvl_selected
-) ; end
-})
-buttonRinascimento:addEventListener( "tap", onObjectTap )
+    defaultFile ="img/sfondo_rinascimento.png",
+    onPress = function() lvl_selected
+  = "rinascimento";composer.setVariable("lvl_selected",lvl_selected
+  ) ; end
+  })
+  buttonRinascimento:addEventListener( "tap", onObjectTap )
 
 
-local buttonRisorgimento = widget.newButton(
-{
-  width = lunghezzaLivelli,
-  height = altezzaLivelli,
-  label = "Risorgimento 1700 D.C.",
-  textAllign ="center",
-  labelColor = { default={ 0.2, 0.2, 0.2  }, over={ 0, 0, 0, 0.5 } },
-  labelYOffset = altezzaLivelli/2 + 20,
-  top = 20,
-  left = 20,
-  defaultFile ="img/sfondo_risorgimento.png",
-  onPress = function() lvl_selected
-= "risorgimento";composer.setVariable("lvl_selected",lvl_selected
-) ; end
-
-})
-
-buttonRisorgimento:addEventListener( "tap", onObjectTap )
-
-local button1900 = widget.newButton(
-{
-  width = lunghezzaLivelli,
-  height = altezzaLivelli,
-  label = "Età Moderna 1900 D.C.",
-  myName = "eta_moderna",
-  textAllign ="center",
-  labelColor = { default={ 0.2, 0.2, 0.2  }, over={ 0, 0, 0, 0.5 } },
-  labelYOffset = altezzaLivelli/2 + 20,
-  top = 20,
-  left = 20,
-  defaultFile ="img/sfondo_1900.png",
-  onPress = function() lvl_selected
-= "eta_moderna";composer.setVariable("lvl_selected",lvl_selected
-) ; end
-})
-
-button1900:addEventListener( "tap", onObjectTap )
-
-
---inserimetno pulsanti livelli nella Buttons Table
-table.insert( buttonsTable, buttonMedioevo )
-table.insert( buttonsTable, buttonRinascimento )
-table.insert( buttonsTable, buttonRisorgimento )
-table.insert( buttonsTable, button1900 )
-
---inserimento dei pulsanti nella scrollView
-for i=1,table.maxn( buttonsTable ) do
-  buttonsTable[i].x = i*lunghezzaLivelli + (i-1)*100
-  scrollView:insert(buttonsTable[i])
-end
-
---Pulsante Gioca
-local playButton = widget.newButton(
-    {
-        id = "playButton",
-        label = "Play",
-        numero = 3,
-        emboss = false,
-        labelColor = { default={ 1, 1, 1 }, over={ 1, 0, 0, 0.5 } },
-        -- Properties for a rounded rectangle button
-        shape = "roundedRect",
-        width = 70,
-        height = 70,
-        cornerRadius = 20,
-        fillColor = { default={0.1,0.4,0.8,0.8}, over={0.1,0.2,0.4,0.6} },
-        strokeColor = { default={0,0.2,0.4,0.8}, over={0.8,0.8,1,1} },
-        strokeWidth = 4
-    }
-)
-playButton.x = display.contentWidth- 5
-playButton.y = display.contentHeight - playButton.height
-playButton:addEventListener("touch",letsPlay)
-
-uiGroup:insert(playButton)
-
-local indietroButton = widget.newButton(
+  local buttonRisorgimento = widget.newButton(
   {
-    label = "Back",
-    onRelease = previousPage,
-    emboss = false,
-    labelColor = { default={ 1, 1, 1 }, over={ 1, 0, 0, 0.5 } },
-    -- Properties for a rounded rectangle button
-    shape = "roundedRect",
-    width = 70,
-    height = 70,
-    cornerRadius = 20,
-    fillColor = { default={0.1,0.4,0.8,0.8}, over={0.1,0.2,0.4,0.6} },
-    strokeColor = { default={0,0.2,0.4,0.8}, over={0.8,0.8,1,1} },
-    strokeWidth = 4
+    width = lunghezzaLivelli,
+    height = altezzaLivelli,
+    label = "Risorgimento 1700 D.C.",
+    textAllign ="center",
+    labelColor = { default={ 0.2, 0.2, 0.2  }, over={ 0, 0, 0, 0.5 } },
+    labelYOffset = altezzaLivelli/2 + 20,
+    top = 20,
+    left = 20,
+    defaultFile ="img/sfondo_risorgimento.png",
+    onPress = function() lvl_selected
+  = "risorgimento";composer.setVariable("lvl_selected",lvl_selected
+  ) ; end
 
-  }
-)
-indietroButton.x = 5
-indietroButton.y = playButton.y 
-uiGroup:insert(indietroButton)
+  })
+
+  buttonRisorgimento:addEventListener( "tap", onObjectTap )
+
+  local button1900 = widget.newButton(
+  {
+    width = lunghezzaLivelli,
+    height = altezzaLivelli,
+    label = "Età Moderna 1900 D.C.",
+    myName = "eta_moderna",
+    textAllign ="center",
+    labelColor = { default={ 0.2, 0.2, 0.2  }, over={ 0, 0, 0, 0.5 } },
+    labelYOffset = altezzaLivelli/2 + 20,
+    top = 20,
+    left = 20,
+    defaultFile ="img/sfondo_1900.png",
+    onPress = function() lvl_selected
+  = "eta_moderna";composer.setVariable("lvl_selected",lvl_selected
+  ) ; end
+  })
+
+  button1900:addEventListener( "tap", onObjectTap )
+
+
+  --inserimetno pulsanti livelli nella Buttons Table
+  table.insert( buttonsTable, buttonMedioevo )
+  table.insert( buttonsTable, buttonRinascimento )
+  table.insert( buttonsTable, buttonRisorgimento )
+  table.insert( buttonsTable, button1900 )
+
+  --inserimento dei pulsanti nella scrollView
+  for i=1,table.maxn( buttonsTable ) do
+    buttonsTable[i].x = i*lunghezzaLivelli + (i-1)*100
+    scrollView:insert(buttonsTable[i])
+  end
+
+  --Pulsante Gioca
+  local playButton = widget.newButton(
+      {
+          id = "playButton",
+          label = "Play",
+          numero = 3,
+          emboss = false,
+          labelColor = { default={ 1, 1, 1 }, over={ 1, 0, 0, 0.5 } },
+          -- Properties for a rounded rectangle button
+          shape = "roundedRect",
+          width = 70,
+          height = 70,
+          cornerRadius = 20,
+          fillColor = { default={0.1,0.4,0.8,0.8}, over={0.1,0.2,0.4,0.6} },
+          strokeColor = { default={0,0.2,0.4,0.8}, over={0.8,0.8,1,1} },
+          strokeWidth = 4
+      }
+  )
+  playButton.x = display.contentWidth- 5
+  playButton.y = display.contentHeight - playButton.height
+  playButton:addEventListener("touch",letsPlay)
+
+  uiGroup:insert(playButton)
+
+  local indietroButton = widget.newButton(
+    {
+      label = "Back",
+      onRelease = previousPage,
+      emboss = false,
+      labelColor = { default={ 1, 1, 1 }, over={ 1, 0, 0, 0.5 } },
+      -- Properties for a rounded rectangle button
+      shape = "roundedRect",
+      width = 70,
+      height = 70,
+      cornerRadius = 20,
+      fillColor = { default={0.1,0.4,0.8,0.8}, over={0.1,0.2,0.4,0.6} },
+      strokeColor = { default={0,0.2,0.4,0.8}, over={0.8,0.8,1,1} },
+      strokeWidth = 4
+
+    }
+  )
+  indietroButton.x = 5
+  indietroButton.y = playButton.y 
+  uiGroup:insert(indietroButton)
 
 end
 
