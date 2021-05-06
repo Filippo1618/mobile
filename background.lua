b3ba4db505139c039e70baca732d4fdb052bb149
WaFlaco <falcone.valerioag@gmail.com>
2021-05-06 11:04:36 +0200

    ops, di nuovo

--- background.lua
+++
@@ -1,8 +0,0 @@
----@diagnostic disable: undefined-global
-function NewBackground (pngPath)
-    local self  = display.newGroup()
-    local bg = display.newImageRect(self,pngPath,DisplayWidth*1.2,DisplayHeight*1.2)
-    bg.x = display.contentCenterX
-    bg.y = display.contentCenterY
-    return self
-end
