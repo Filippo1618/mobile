b3c9957c57309f8702892de323ad0aa17de07330
Valerio Agostino Falcone <44167264+WaFlaco@users.noreply.github.com>
2021-05-14 11:36:20 +0200

    Update lvlCharTable.lua

--- lvlCharTable.lua
+++ lvlCharTable.lua
@@ -8,14 +8,14 @@ local composer = require("composer")
 
 local characters = composer.getVariable("characters")
 
-local lvl_charTable = {}
-lvl_charTable["nessuno"] = { characters[1] }
-lvl_charTable["medioevo"] = { characters[2],characters[2],characters[3] }
-lvl_charTable["rinascimento"] = { characters[3],characters[3],characters[4] }
-lvl_charTable["risorgimento"] = { characters[4],characters[4],characters[5] }
-lvl_charTable["eta_moderna"] = { characters[5],characters[5],characters[5] }
+local lvlCharTable = {}
+lvlCharTable["nessuno"] = { characters[1] }
+lvlCharTable["medioevo"] = { characters[2],characters[2],characters[3] }
+lvlCharTable["rinascimento"] = { characters[3],characters[3],characters[4] }
+lvlCharTable["risorgimento"] = { characters[4],characters[4],characters[5] }
+lvlCharTable["eta_moderna"] = { characters[5],characters[5],characters[5] }
 
-local start_lvl = lvl_charTable["nessuno"]
+local startLvl = lvlCharTable["nessuno"]
 
-composer.setVariable("lvl_charTable", lvl_charTable)
-composer.setVariable("start_lvl", start_lvl)
+composer.setVariable("lvlCharTable", lvlCharTable)
+composer.setVariable("startLvl", startLvl)
